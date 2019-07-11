cd6f81e1a2362f45d677c91d5bd74f7b44fafe75
harapan <harapanpardamean@gmail.com>
2019-07-11 20:03:22 +0700

    .

--- src/setup/index.tsx
+++ src/setup/index.tsx
@@ -2,16 +2,35 @@ import * as React from 'react';
 import style from '../../public/style.scss';
 import Started from '../../public/start.svg';
 import {Formik, Field, Form} from 'formik';
+import * as Yup from 'yup';
 
 const InputLarge = ({
   field,
   form: {touched, errors},
   ...props
 }: FieldProps<UserSetup>) => {
+  const isOverflow = field.value ? (field.value.length > 15 ? true : false) : 0;
   return (
-    <div>
+    <div className={`${style.formLarge} ${isOverflow ? style.overflow : ''}`}>
       <label>{props.label}</label>
-      <input type={props.type} {...field} placeholder="type here" />
+      <input
+        maxLength={25}
+        type={props.type}
+        {...field}
+        placeholder="type here"
+      />
+
+      <button
+        type="button"
+        style={{
+          fontSize: '.825rem',
+          all: 'unset',
+          cursor: 'pointer',
+          color: 'var(--pink)',
+        }}
+      >
+        next
+      </button>
     </div>
   );
 };
@@ -21,7 +40,11 @@ const Landing = ({onClick}: any) => {
     <>
       <h1>Make smile manage your content</h1>
       <Started />
-      <button onClick={onClick} style={{all: 'unset', cursor: 'pointer'}}>
+      <button
+        type="button"
+        onClick={onClick}
+        style={{all: 'unset', cursor: 'pointer'}}
+      >
         <h2 style={{fontWeight: 500, color: 'var(--pink)'}}>Get Started</h2>
       </button>
     </>
@@ -36,17 +59,21 @@ export default () => {
     passwordUsr: '',
     avatar: '',
   });
+  const userSetupSchema = Yup.object().shape({
+    fullName: Yup.string().required(),
+    userName: Yup.string().required(),
+    emailAddress: Yup.string()
+      .email()
+      .required(),
+    passwordUsr: Yup.string()
+      .required()
+      .matches(
+        '^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$',
+        'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character',
+      ),
+    avatar: Yup.string().required(),
+  });
   const [stepIndex, setIndex] = React.useState(0);
-  const handleState = () => {
-    if (stepIndex > Page.length) {
-      setIndex(0);
-    }
-    setIndex((state: number) => state + 1);
-  };
-  const Page: React.ReactNode[] = [
-    <Landing key={0} onClick={() => handleState()} />,
-    <Field key={1} label="What is your name?" components={InputLarge} />,
-  ];
   const handleSetup = (changedState: UserSetup) => {
     setSetup(changedState);
   };
@@ -54,12 +81,31 @@ export default () => {
     <div className={style.setup}>
       <Formik
         initialValues={initialState}
+        validationSchema={userSetupSchema}
         onSubmit={(values: UserSetup) => {
           alert(JSON.stringfy(values));
         }}
         render={(formikBag: FormikProps<UserSetup>) => (
           <Form style={{all: 'inherit'}} id="setupForm">
-            {Page[stepIndex]}
+            {(() => {
+              const handleState = () => {
+                if (stepIndex > Page.length) {
+                  setIndex(0);
+                }
+                setIndex((state: number) => state + 1);
+              };
+              const Page: React.ReactNode[] = [
+                <Landing key={0} onClick={() => handleState()} />,
+                <Field
+                  key={1}
+                  type="text"
+                  name="fullName"
+                  label="What is your name?"
+                  component={InputLarge}
+                />,
+              ];
+              return Page[stepIndex];
+            })()}
           </Form>
         )}
       />
