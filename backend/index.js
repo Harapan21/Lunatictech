4b62e2f0233cfa0f68f8baf96f26fd138145e622
harapan <harapanpardamean@gmail.com>
2019-09-17 01:41:19 +0700

    add rust to backend

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {ApolloServer} = require('apollo-server-express');
-const {VerifyAuth} = require('./src/db');
+const { ApolloServer } = require('apollo-server-express');
+const { VerifyAuth } = require('./src/db');
 const typeDefs = require('./src/schema');
 const resolvers = require('./src/resolvers');
 const http = require('http');
 const cors = require('cors');
 const fs = require('fs');
 const rimraf = require('rimraf');
-const {isValid} = require('./src/db');
-(async () => {
-  fs.readdirSync('uploads', {
-    withFileTypes: true,
-  }).map(async dir => {
+const { isValid } = require('./src/db');
+const native = require('./native');
+const removeFile = async () => {
+  native.check_garbage_upload().map(async dir => {
     try {
-      if (!dir.isFile()) {
-        const {username} = await isValid(dir.name);
-        if (!username) {
-          console.log(`␡ ${dir.name}`);
-          rimraf(`./uploads/${dir.name}/`, e => {
-            console.log(e);
-          });
-        }
+      const { username } = await isValid(dir);
+      if (!username) {
+        console.log(native.remove_dir(`./uploads/${dir}`));
       }
     } catch (e) {
       console.log(e);
     }
   });
-})();
+};
+removeFile();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({req}) => {
+  context: ({ req }) => {
     if (req.headers && req.headers.token) {
       return VerifyAuth(req.headers.token);
     }
@@ -39,13 +34,14 @@ const server = new ApolloServer({
   },
   uploads: {
     maxFileSize: 10000000, // 10 MB
-    maxFiles: 20,
-  },
+    maxFiles: 20
+  }
 });
+
 const app = express();
 app.use('/static', express.static('uploads'));
 app.use(bodyParser.json());
 app.use(cors());
-server.applyMiddleware({app, path: '/graphql'});
+server.applyMiddleware({ app, path: '/graphql' });
 
 http.createServer(app).listen(4000);
--- backend/native/src/lib.rs
+++ backend/native/src/lib.rs
@@ -4,12 +4,24 @@ extern crate neon;
 extern crate neon_serde;
 extern crate serde_derive;
 extern crate num_cpus;
+use std::fs;
+
+
+
+
 
 export!{
     fn hello() -> String {
         String::from("hello")
     }
-
+    fn check_garbage_upload() -> Vec<String>{
+        let upload_file = fs::read_dir("./uploads").unwrap();
+        upload_file.into_iter().filter(|ref x| x.as_ref().unwrap().path().is_dir()).map(|entry| entry.unwrap().file_name().to_str().unwrap().into()).collect()
+    }
+    fn remove_dir(path: String) -> String {
+        fs::remove_dir(path.to_owned()).unwrap();
+        format!("Removing garbage dir : {}",path.to_owned())
+    }
     fn thread_count() -> f64 {
        num_cpus::get() as f64
     }
