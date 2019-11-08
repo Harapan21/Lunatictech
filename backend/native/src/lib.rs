64c875aa49193f304abd794e8d0389ef067a9298
Harapan21 <harapanpardamean@gmail.com>
2019-11-09 00:54:38 +0700

    not finish yet to implement diesel

--- backend/native/src/lib.rs
+++ backend/native/src/lib.rs
@@ -1,43 +1,48 @@
 #![allow(non_snake_case)]
+extern crate chrono;
+#[macro_use]
 extern crate neon;
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
 #[macro_use]
 extern crate neon_serde;
+extern crate serde_json;
+#[macro_use]
 extern crate serde_derive;
 use std::fs;
 
+use chrono::prelude::*;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use neon::prelude::*;
 use neon::register_module;
 use std::env;
 
+mod db;
+mod handlefile;
 mod models;
 mod schema;
 
-pub fn establish_connection() -> MysqlConnection {
-    dotenv().ok();
+register_module!(mut m, {
+    m.export_function("check_garbage_upload", handlefile::check_garbage_upload)?;
+    m.export_function("remove_dir", handlefile::remove_dir)?;
+    Ok(())
+});
 
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    MysqlConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
+// export! {
+//     fn check_garbage_upload() -> Vec<String> {
+//         let upload_file = fs::read_dir("./uploads").unwrap();
+//         upload_file
+//             .into_iter()
+//             .filter(|ref x| x.as_ref().unwrap().path().is_dir())
+//             .map(|entry| entry.unwrap().file_name().to_str().unwrap().into())
+//             .collect()
+//     }
 
-export! {
-    fn check_garbage_upload() -> Vec<String> {
-        let upload_file = fs::read_dir("./uploads").unwrap();
-        upload_file
-            .into_iter()
-            .filter(|ref x| x.as_ref().unwrap().path().is_dir())
-            .map(|entry| entry.unwrap().file_name().to_str().unwrap().into())
-            .collect()
-    }
+//     fn remove_dir(path: String) -> String {
+//         fs::remove_dir_all(path.to_owned()).unwrap();
+//         format!("Removing garbage dir : {}",path.to_owned())
+//     }
 
-    fn remove_dir(path: String) -> String {
-        fs::remove_dir_all(path.to_owned()).unwrap();
-        format!("Removing garbage dir : {}",path.to_owned())
-    }
-
-}
+// }
--- backend/native/src/models.rs
+++ backend/native/src/models.rs
@@ -1,14 +1,33 @@
-use diesel::sql_types::{Bool, Timestamp};
-
-#[derive(Queryable)]
+use crate::*;
+use schema::usr_smile;
+#[derive(Serialize, Queryable, PartialEq)]
 pub struct User {
     pub user_id: String,
     pub username: String,
     pub email: String,
-    pub jointAt: Timestamp,
-    pub lastEditedAt: Timestamp,
+    pub jointAt: NaiveDateTime,
+    pub lastEditedAt: NaiveDateTime,
     pub fullname: String,
     pub avatar: String,
     pub password: String,
-    pub isAdmin: Bool,
+    pub isAdmin: bool,
+}
+
+#[derive(Insertable)]
+#[table_name = "usr_smile"]
+pub struct LoginField<'a> {
+    username: &'a str,
+    password: &'a str,
+}
+
+#[derive(Insertable)]
+#[table_name = "usr_smile"]
+pub struct UserField<'a> {
+    pub user_id: &'a str,
+    pub username: &'a str,
+    pub email: &'a str,
+    pub fullname: &'a str,,
+    pub avatar: &'a str,
+    pub password: String,
+    pub isAdmin: bool,
 }
